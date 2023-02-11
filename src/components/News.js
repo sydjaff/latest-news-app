7319352e713490ddd47da26e1ab04ca148a73e4d
sydjaff <sydjaff07@gmail.com>
2023-02-11 14:34:28 +0500

    commit

---
+++ src/components/News.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+export default function News() {
+  const [newsArticles, setNewsArticels] = useState([]);
+  const [pageNo, setPageNo] = useState(1);
+  const [totalResults, setTotalResults] = useState();
+
+  // api call
+  const fetchNews = async (pageNo) => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=7db84bc3b09c46948a34dc761c4de818&page=${pageNo}&pageSize=10`;
+    let response = await fetch(url);
+    let news = await response.json();
+    setNewsArticels(news.articles);
+    setTotalResults(news.totalResults);
+  };
+
+  // prev page func
+  const handlePrevClick = () => {
+    console.log("prev");
+    setPageNo(pageNo - 1);
+  };
+  // next page fun
+  const handleNextClick = () => {
+    console.log("next");
+    setPageNo(pageNo + 1);
+  };
+
+  useEffect(() => {
+    fetchNews(pageNo);
+  }, [pageNo]);
+
+  // useEffect(() => {
+  //   console.log(
+  //     `total: ${totalResults}, size: ${pageSize}, number: ${pageNo}, next: ${
+  //       totalResults / pageSize < pageNo
+  //     }, previous: ${pageNo == 1}`
+  //   );
+  // }, [totalResults, pageNo, pageSize]);
+
+  return (
+    <div>
+      <div className="container my-3">
+        <h1>GetNews - Top headline</h1>
+        <div className="row">
+          {newsArticles?.map((ar) => {
+            return (
+              <div className="col-md-3 my-2" key={ar.url}>
+                <NewsItem
+                  title={ar.title}
+                  description={ar.description}
+                  urlToImage={ar.urlToImage}
+                  newsUrl={ar.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-end gap-2">
+          <button
+            onClick={handlePrevClick}
+            type="button"
+            className="btn btn-dark"
+            disabled={pageNo === 1}
+          >
+            &larr; Previous
+          </button>
+          <button
+            onClick={handleNextClick}
+            type="button"
+            className="btn btn-dark"
+            disabled={pageNo + 1 > Math.ceil(totalResults / 10)}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function NewsItem(props) {
+  return (
+    <div>
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src={
+            !props.urlToImage
+              ? "https://english.olympiadsuccess.com/assets/images/english_square/articles-4.jpg"
+              : props.urlToImage
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">
+            {props.title ? props.title.slice(0, 45) : ""}...
+          </h5>
+          <p className="card-text">
+            {props.description ? props.description.slice(0, 70) : ""}...
+          </p>
+          <a
+            rel="noreferrer"
+            href={props.newsUrl}
+            target="_blank"
+            className="btn btn-dark btn-sm"
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
