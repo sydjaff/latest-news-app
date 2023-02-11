9663bd712ff96ce4e978ea358e47f50f93fc1743
sydjaff <sydjaff07@gmail.com>
2023-02-11 16:00:40 +0500

    add loader

--- src/components/News.js
+++ src/components/News.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
-export default function News() {
+
+export default function News(props) {
+
   const [newsArticles, setNewsArticels] = useState([]);
   const [pageNo, setPageNo] = useState(1);
   const [totalResults, setTotalResults] = useState();
-
+  const [loading, setLoading] = useState(false);
   // api call
   const fetchNews = async (pageNo) => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=7db84bc3b09c46948a34dc761c4de818&page=${pageNo}&pageSize=10`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=7db84bc3b09c46948a34dc761c4de818&page=${pageNo}&pageSize=${props.pageSize}`;
     let response = await fetch(url);
+    setLoading(true);
     let news = await response.json();
+    if (news) { setLoading(false) }
     setNewsArticels(news.articles);
     setTotalResults(news.totalResults);
   };
@@ -28,26 +33,40 @@ export default function News() {
 
   useEffect(() => {
     fetchNews(pageNo);
+    setLoading(true)
   }, [pageNo]);
 
+  useEffect(() => {
+    fetchNews(pageNo);
+    setLoading(true)
+  }, []);
   return (
+
     <div>
       <div className="container my-3">
-        <h1>GetNews - Top headline</h1>
-        <div className="row">
-          {newsArticles?.map((ar) => {
-            return (
-              <div className="col-md-3 my-2" key={ar.url}>
-                <NewsItem
-                  title={ar.title}
-                  description={ar.description}
-                  urlToImage={ar.urlToImage}
-                  newsUrl={ar.url}
-                />
-              </div>
-            );
-          })}
-        </div>
+        <h1 className="text-center">GetNews - Top headline</h1>
+        {
+          loading ? (
+            <Spinner />
+          ) : (
+            <div className="row">
+              {newsArticles?.map((ar) => {
+                return (
+                  <div className="col-md-3 my-2" key={ar.url}>
+                    <NewsItem
+                      title={ar.title}
+                      description={ar.description}
+                      urlToImage={ar.urlToImage}
+                      newsUrl={ar.url}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          )
+        }
+
+
         <div className="container d-flex justify-content-end gap-2">
           <button
             onClick={handlePrevClick}
@@ -61,7 +80,7 @@ export default function News() {
             onClick={handleNextClick}
             type="button"
             className="btn btn-dark"
-            disabled={pageNo + 1 > Math.ceil(totalResults / 10)}
+            disabled={pageNo + 1 > Math.ceil(totalResults / props.pageSize)}
           >
             Next &rarr;
           </button>
